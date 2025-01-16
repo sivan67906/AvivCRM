4b20f05804da7c2a5b2cd6af127bc1a98364bbbf
Sivan T <Sivan T@DESKTOP-6PN6KNF>
2025-01-16 15:52:14 +0530

    login page included

---
+++ AvivCRM.UI/Controllers/SignupController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AvivCRM.UI.Controllers;
+
+public class SignupController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Signup()
+    {
+        return View();
+    }
+}
